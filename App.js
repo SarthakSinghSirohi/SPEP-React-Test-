b6d409d27fc37844eddd638f5557e2bb7c188470
SarthakSinghSirohi <94757058+SarthakSinghSirohi@users.noreply.github.com>
2023-06-19 09:16:53 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import Tabs from './Components/Tabs';
+
+function App() {
+  return (
+    <div>
+      <h1></h1>
+      <Tabs
+        tabs={[
+          { title: 'Tab 1', content: 'This is the content for Tab 1' },
+          { title: 'Tab 2', content: 'This is the content for Tab 2' },
+          { title: 'Tab 3', content: 'This is the content for Tab 3' }
+        ]}
+      />
+    </div>
+  );
+}
+
+export default App;
